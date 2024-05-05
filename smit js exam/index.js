9a80f6615239b8a5b72a066c476f638e6fda875d
aashir12 <91082718+aashir12@users.noreply.github.com>
2024-05-05 14:17:28 +0500

    Add files via upload

---
+++ smit js exam/index.js
@@ -0,0 +1,90 @@
+// Question Number 1
+
+// const iterator=(arr)=>{
+//     for(var i=0;i<arr.length;i++){
+//         if(arr[i]<10){
+//             console.log(-1)
+//         }
+//         else{
+//              console.log(arr[i]+' at '+i)
+//         }
+//     }
+// }
+
+// console.log(iterator([10,1,2,1,10]))
+
+// // Question Number 2
+
+// const average=(arr)=>{
+//     sum=0;
+//     for(var i=0;i<arr.length;i++){
+//      sum+=arr[i];
+//     }
+//     return sum/arr.length
+// }
+
+// console.log(average([5,10,15,20]))
+
+// // Question Number 3
+
+// var button=document.getElementById('button');
+// button.addEventListener('click',()=>{
+//     alert('Button Clicked!')
+// })
+
+// //Question Number 4
+
+// const lengthCalc=(arr)=>{
+//     var joinedArr=arr.join(' ');
+//     return joinedArr.length;
+// }
+
+// console.log("the calc of this "+lengthCalc(["This", "is" ,"Ali"]) )
+
+// Question Number 5
+
+
+
+// const findMax=(arr)=>{
+//     var num=0; //10
+// for(var i=0;i<arr.length;i++){
+//     if(num<arr[i]){
+//           num=arr[i]
+//     }
+    
+// }
+// alert(num)
+// }
+
+// findMax([10,5,39,6,25,26,29])
+
+// Question Number 6
+// var button=document.getElementById('colorButton');
+// var num=0;
+// button.addEventListener('click',()=>{
+//     if(num==0){
+//         document.body.style.background='blue'
+//         num=1;
+//     }
+//     else{
+//         document.body.style.background='white'
+//         num=0;
+//     }
+// })
+
+
+// Question Number 8
+
+// const numChecker=(number)=>{
+//     if(number===0){
+//         return null;
+//     }
+//     else if(number%2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(numChecker(0))
